462d5f31a41407357342589399343b6904ee0d8e
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-10-20 00:55:09 +0000

    Terceiro arquivos

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ print("Hello Word!!")
 
 # Quando quiser guardar uma String!(fase)
 nome = "Samuel Lopes"
+
 # Quando quiser guardar número inteiro 
 idade = 19 
 
 #Exibir o meu nome (que está dentro da variável nome)
-print("My name is" ,nome, "is " 
-       "My age is ",idade," years old")
+
+print("My name is" ,nome, "and " 
+       "My age is ",idade,"years old")
 
 '''
 
