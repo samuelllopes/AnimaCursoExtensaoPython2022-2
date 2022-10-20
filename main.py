54b4cf81110d6ea23d4f47edb0d236155fe8cbdc
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-10-20 00:52:29 +0000

    Segundos arquivos

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # Meu primeiro projeto Python!! 
-
+#
 # print() = comando de saida 
 print("Hello Word!!")
 
+# Quando quiser guardar uma String!(fase)
+nome = "Samuel Lopes"
+# Quando quiser guardar número inteiro 
+idade = 19 
+
+#Exibir o meu nome (que está dentro da variável nome)
+print("My name is" ,nome, "is " 
+       "My age is ",idade," years old")
 
 '''
 
 Comentario em Bloco  
 
 '''
-
