11873d7cc85f510fe01882714291bcc72e9767d2
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-10-26 23:08:59 +0000

    Sexto Arquivo

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Projeto
+# Projeto Python - Extensão 
+
+# Receber algo do usuário input()
+nome = input("Digite seu nome: ")
+idade = int(input("Qual sua idade: "))
+
+print(f"Boa Noite, seu nome é {nome} e você tem {idade} anos.")
+
+dobro = idade * 2
+print(f"O dobro da sua idade é {dobro}.")
